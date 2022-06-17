31cd631a03d32d62336130c41e3df645ec0702c4
Omer-Cicek <omercicek160408@gmail.com>
2022-06-17 17:46:42 +0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid
       :quotes="quotes"
@@ -16,6 +17,7 @@
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
 import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 export default {
   data() {
     return {
@@ -26,9 +28,12 @@ export default {
   components: {
     appQuoteGrid: QuoteGrid,
     appNewQuote: NewQuote,
+    appHeader: Header,
   },
   methods: {
     newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes)
+        return alert('You need to delete first!');
       this.quotes.push(quote);
     },
     deletingQuote(index) {
